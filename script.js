d9bace306f05d43f9fe34475b851b7230bb9cd30
Maria Aluko <maria.alukoo@gmail.com>
2024-10-03 09:53:05 +0300

    add comments to step 1 of Pancake

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const pancakePriceCalc = () => {
     totalPriceElement.textContent = `${total.toFixed(0)}€`;
 
     const priceBanner = document.querySelector('.price-banner');
-    priceBanner.animate(
+    priceBanner.animate( // add an animation to price change in .price-banner
         [
             { transform: 'scale(1)' },
             { transform: 'scale(1.05)' },
@@ -20,13 +20,13 @@ const pancakePriceCalc = () => {
         ],
         {
             duration: 100,
-            iterations: 1,
+            iterations: 1, // iterate once
         }
     );
 };
 
 const checkToppings = () => {
-    for (const item of checkboxes) {
+    for (const item of checkboxes) { // for each item if checked, add to total
         if (item.checked) {
             const itemPrice = parseInt(item.value) || 0;
             total += itemPrice;
